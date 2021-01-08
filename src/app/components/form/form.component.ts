a505c85df437365f7cc0e15adbee4886e8fd32c6
Andre Alves <andrealvesdeoliveira@outlook.com>
2021-01-07 21:53:44 -0300

    Sincronizado HTML comFormGroup e Validators

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form',
@@ -7,9 +8,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormComponent implements OnInit {
 
-  constructor() { }
+  formulario: FormGroup;
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.formulario = this.fb.group({
+      nome: [null, [Validators.required]],
+      cpf: [null,[Validators.required]],
+      rg: [null,[Validators.required]],
+      dataNascimento: [null,[Validators.required]],
+      sexo: ['Masculino', 'Feminino',[Validators.required]],
+      nomeMae: [null,[Validators.required]],
+      nomePai: [null,[Validators.required]],
+      endereco: [null,[Validators.required]],
+      email: [null,[Validators.required, Validators.email]],
+      tel: [null,[Validators.required]]
+    })
   }
 
+  cpfMask: string = '000.000.000-00';
+  rgMask: string = '00.000.000-0';
+  telMask: string = '(00) 0 0000-0000';
+
+  onSubmit(){
+    console.warn(this.formulario.value);
+  }
 }

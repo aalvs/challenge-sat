1f7874549bdaf04b1bead9a4b674c95cbfeddcc4
Andre Alves <andrealvesdeoliveira@outlook.com>
2021-01-08 23:50:48 -0300

    Removido campos e justado estilização do form

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -1,6 +1,6 @@
+import { FormResourceService } from './../../shared/services/form-resource.service';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
@@ -10,28 +10,49 @@ export class FormComponent implements OnInit {
 
   formulario: FormGroup;
 
-  constructor(private fb: FormBuilder) { }
+  listaColabs: Object[];
+
+  constructor(
+    private fb: FormBuilder,
+    private service: FormResourceService
+    ) { }
 
   ngOnInit(): void {
+
+    this.listaColabs = [
+      {
+        nome: 'André',
+        cpf: '000.000.000-00',
+        rg: '00.000.000-0',
+        dataNascimento: '04/07/1995',
+        email: 'andre@email.com',
+        tel: '(00) 0 0000-0000'
+      }
+    ];
+
+
+    this.service.listar()
+      .subscribe(resposta => this.listaColabs = resposta);
+
+
     this.formulario = this.fb.group({
       nome: [null, [Validators.required]],
       cpf: [null,[Validators.required]],
       rg: [null,[Validators.required]],
       dataNascimento: [null,[Validators.required]],
       sexo: ['Masculino',[Validators.required]],
-      nomeMae: [null,[Validators.required]],
-      nomePai: [null,[Validators.required]],
-      endereco: [null,[Validators.required]],
       email: [null,[Validators.required, Validators.email]],
       tel: [null,[Validators.required]]
-    })
+    });
   }
 
   cpfMask: string = '000.000.000-00';
   rgMask: string = '00.000.000-0';
+  dateMask: string = '00/00/0000';
   telMask: string = '(00) 0 0000-0000';
 
   onSubmit(){
+    console.log(this.formulario);
     console.warn(this.formulario.value);
     this.formulario.reset();
   }

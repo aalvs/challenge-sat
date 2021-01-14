fab63993ea77af8b2999abfd7646d7b839aade2d
Andre Alves <andrealvesdeoliveira@outlook.com>
2021-01-13 21:18:31 -0300

    Criado service para o formulário

--- src/app/shared/services/form-resource.service.ts
+++ src/app/shared/services/form-resource.service.ts
@@ -1,12 +1,28 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Colaborador } from '../models/colaborador.model';
 @Injectable({
   providedIn: 'root'
 })
 export class FormResourceService {
 
-  apiUrl = 'http://localhost:8080/colaboradores';
+  apiUrl = 'https://challenge-sat-apirest.herokuapp.com/api';
 
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json'
+    })
+  };
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  public listar(): Observable<Colaborador> {
+    return this.http.get<Colaborador>(`${this.apiUrl}/pessoas`);
+  }
+
+  public criar(colaborador: Colaborador): Observable<Colaborador> {
+    return this.http.post<Colaborador>(`${this.apiUrl}/pessoa`, colaborador, this.httpOptions);
+  }
+
 }

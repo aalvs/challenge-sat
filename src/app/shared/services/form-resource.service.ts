f9fc1bc7d052ac1ff3b733367217abe1e7410196
Andre Alves <andrealvesdeoliveira@outlook.com>
2021-01-16 14:30:39 -0300

    Finalizado metodo de tratamento das requisições

--- src/app/shared/services/form-resource.service.ts
+++ src/app/shared/services/form-resource.service.ts
@@ -1,28 +1,41 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
 import { Colaborador } from '../models/colaborador.model';
+import { environment } from 'src/environments/environment';
 @Injectable({
   providedIn: 'root'
 })
 export class FormResourceService {
 
-  apiUrl = 'https://challenge-sat-apirest.herokuapp.com/api';
+  apiUrl = environment.apiUrl;
 
   httpOptions = {
     headers: new HttpHeaders({
-      'Content-Type': 'application/json'
+      'Content-Type': 'application/json',
+      Authorization: 'my-auth-token'
     })
   };
 
   constructor(private http: HttpClient) { }
 
   public listar(): Observable<Colaborador> {
-    return this.http.get<Colaborador>(`${this.apiUrl}/pessoas`);
+    return this.http.get<Colaborador>(`${this.apiUrl}pessoas`)
   }
 
-  public criar(colaborador: Colaborador): Observable<Colaborador> {
-    return this.http.post<Colaborador>(`${this.apiUrl}/pessoa`, colaborador, this.httpOptions);
+  public criar(colaborador: any): Observable<Colaborador> {
+    return this.http.post<any>(`${this.apiUrl}pessoa`, colaborador, this.httpOptions);
   }
 
+  private tratarErrors(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+      console.error('Ocorreu um erro:', error.error.message);
+    } else {
+      console.error(
+        `O código retornado pelo nack-end é ${error.status},`+
+        `O corpo da mensagem é: ${error.error}`
+      );
+    }
+    return throwError('Algo ruim aconteceu. Tente novamente mais tarde.');
+  }
 }
